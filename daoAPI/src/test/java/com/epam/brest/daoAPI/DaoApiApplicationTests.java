81a52029d24cdf6a7390e85b8a84801f242ccc13
Oleg Suhodolsky <olegsuhodolsky@gmail.com>
2022-02-17 21:51:44 +0300

    DAO API INJECTED

---
+++ daoAPI/src/test/java/com/epam/brest/daoAPI/DaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.brest.daoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
