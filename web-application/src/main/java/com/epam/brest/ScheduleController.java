71079014e048ef6c750ea819ae93610f372e141d
Oleg Suhodolsky <olegsuhodolsky@gmail.com>
2022-03-31 10:20:19 +0300

    Template Schedule for all students

--- web-application/src/main/java/com/epam/brest/ScheduleController.java
+++ web-application/src/main/java/com/epam/brest/ScheduleController.java
@@ -4,6 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ScheduleController {
@@ -13,6 +17,17 @@ public class ScheduleController {
     @Autowired
     private ScheduleDtoServiceApi scheduleDtoService;
 
+    @GetMapping(value = "/schedule")
+    public final String groups (Model model) {
+        List<List<StudentsSchedule>> studentsSchedule = scheduleDtoService.getScheduleForAllGroupsService();
+        model.addAttribute("studentsSchedule", scheduleDtoService.getScheduleForAllGroupsService());
+        return "allgroupsschedule";
+    }
 
+    @GetMapping(value = "/createschedule")
+    public final String schedule () {
+        scheduleDtoService.createScheduleService();
+        return "redirect:/lectors";
+    }
 
 }
