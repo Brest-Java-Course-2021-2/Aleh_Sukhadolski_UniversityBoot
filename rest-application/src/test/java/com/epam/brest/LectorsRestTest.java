a52a4afb4d989c73f78989f3788ff9798302960a
Oleg Suhodolsky <olegsuhodolsky@gmail.com>
2022-04-02 21:51:42 +0300

    Application rest tests

--- rest-application/src/test/java/com/epam/brest/LectorsRestTest.java
+++ rest-application/src/test/java/com/epam/brest/LectorsRestTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -43,4 +44,25 @@ public class LectorsRestTest {
         Lector body = this.restTemplate.getForObject("/lectors/lector/{id}", Lector.class, Integer.class );
         assertThat(body.getClass()).isEqualTo(Lector.class);
     }
+
+    @Test
+    public void newLectorTest() {
+        HttpEntity<Lector> request = new HttpEntity<>(new Lector());
+        Lector body = this.restTemplate.postForObject("/lectors/lector/new", request, Lector.class);
+        assertThat(body.getClass()).isEqualTo(Lector.class);
+        assertThat(body != null);
+    }
+
+
+    @Test
+    public void updateLectorTest() {
+        Lector lector = new Lector();
+        lector.setNameLector("Noyy");
+        HttpEntity<Lector> request = new HttpEntity<>(lector);
+        this.restTemplate.put("/lectors/lector/update", request);
+        HttpEntity<String> request1 = new HttpEntity<>("Noyy");
+        Lector body = this.restTemplate.getForObject("/lectors/lector/get-name", Lector.class, String.class);
+        assertThat(body != null);
+    }
+
 }
