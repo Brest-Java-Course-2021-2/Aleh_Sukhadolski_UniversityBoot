d62c8ef7a237b7c5d60884bf0ff058199239ec25
Oleg Suhodolsky <olegsuhodolsky@gmail.com>
2022-06-01 21:57:49 +0300

    Kafka webapp and rest

--- service/src/main/java/com/epam/brest/RequestFromLectorServiceImpl.java
+++ service/src/main/java/com/epam/brest/RequestFromLectorServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
