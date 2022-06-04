73f67fa722de795b97e4ef89dc04e648aca009f8
Oleg Suhodolsky <olegsuhodolsky@gmail.com>
2022-06-04 19:24:21 +0300

    Kafka Schedule ProducerConfig

--- kafka-rest/src/main/java/com/epam/brest/kafkarest/config/producer/GroupKafkaProducerConfigRest.java
+++ kafka-rest/src/main/java/com/epam/brest/kafkarest/config/producer/GroupKafkaProducerConfigRest.java
@@ -20,20 +20,6 @@ public class GroupKafkaProducerConfigRest {
     @Value(value = "${kafka.bootstrapAddress}")
     private String bootstrapAddress;
 
- /*   @Bean
-    public ProducerFactory<String, String> stringGroupProducerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
-
-    @Bean
-    public KafkaTemplate<String, String> stringGroupKafkaTemplate() {
-        return new KafkaTemplate<>(stringGroupProducerFactory());
-    }*/
-
     @Bean
     public ProducerFactory<String, Group> groupProducerFactory() {
         Map<String, Object> configProps = new HashMap<>();
